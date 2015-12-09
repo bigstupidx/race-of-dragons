4949e98878c790f153fbd92f5c93657cad4b7f4c
Thien Le <trongthien18@gmail.com>
2015-12-09 15:45:24 +0700

    Add 4 new map

---
+++ Assets/Scripts/Utility/BackgroundParalaxController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundParalaxController : MonoBehaviour
+{
+    private Camera cam;
+    private float lastCamPosX;
+    private float ratioSpeed = 0.8f;
+    private float minDistanceToMove = 0.5f;
+    private Vector3 correctPos;
+
+    private float smoothTime = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+
+
+    void Start()
+    {
+        cam = Camera.main;
+        lastCamPosX = cam.transform.position.x;
+        correctPos = transform.position;
+    }
+
+    void Update()
+    {
+        float distanceX = cam.transform.position.x - lastCamPosX;
+        if (distanceX >= minDistanceToMove)
+        {
+            correctPos += new Vector3(distanceX * ratioSpeed, 0, 0);
+            lastCamPosX = cam.transform.position.x;           
+        }        
+        transform.position = Vector3.SmoothDamp(transform.position, correctPos, ref velocity, smoothTime);
+    }  
+}
