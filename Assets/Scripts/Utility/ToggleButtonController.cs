476f217a9bcfa779b1c17b1303fc9ca3476da1c1
Thien Le <trongthien18@gmail.com>
2015-12-10 23:56:09 +0700

    Design main menu screen and stor screen (WIP)

---
+++ Assets/Scripts/Utility/ToggleButtonController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleButtonController : MonoBehaviour 
+{
+    private Animator animator;
+    private bool isButtonOn;
+    
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnClick()
+    {        
+        if (!isButtonOn)
+        {
+            animator.Play("Pressed", -1, 0);
+            isButtonOn = true;
+        }
+    }
+
+    public void ResetNormal()
+    {
+        animator.Play("Normal", -1, 0);
+        isButtonOn = false;
+    }
+}
